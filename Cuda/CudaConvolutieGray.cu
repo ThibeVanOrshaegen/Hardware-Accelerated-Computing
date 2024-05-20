bcac3a6be52dbaba10c0327b9b4d8a09826e0eac
Thibe <114075982+ThibeVanOrshaegen@users.noreply.github.com>
2024-05-20 10:50:27 +0200

    Update CudaConvolutieGray.cu

--- Cuda/CudaConvolutieGray.cu
+++ Cuda/CudaConvolutieGray.cu
@@ -36,7 +36,6 @@ __global__ void applyConvolution(unsigned char* image, unsigned char* output, in
                 }
             }
         }
-        /*tot hier*/
         for (int ch = 0; ch < channels; ch++) {
             if (ch < 3) {
                 int val = (int)sum[ch];
